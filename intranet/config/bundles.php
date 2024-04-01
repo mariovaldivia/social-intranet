f44e5ab80b48668e651ace24343e795b5401e768
Mario Valdivia <mvaldivia@nbeta.cl>
2024-03-31 23:46:42 -0300

    thumbnails

--- intranet/config/bundles.php
+++ intranet/config/bundles.php
@@ -17,4 +17,6 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
+    Knp\Bundle\GaufretteBundle\KnpGaufretteBundle::class => ['all' => true],
+    Oneup\FlysystemBundle\OneupFlysystemBundle::class => ['all' => true],
 ];
